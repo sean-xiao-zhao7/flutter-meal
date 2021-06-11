dcd57a2fb37866547cf8230309df3e188348964d
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2021-06-11 15:58:14 -0400

    Use navigator

---
+++ lib/data/categories.dart
@@ -0,0 +1,9 @@
+import '../models/category.dart';
+import 'package:flutter/material.dart';
+
+const CATEGORIES = const [
+  Category(id: 'asdfasdf', title: 'Ch\'King', color: Colors.yellow),
+  Category(id: 'asdf123asdf', title: 'Bacon Sundae', color: Colors.red),
+  Category(id: '123fsadq23', title: '1 Dolla Holla', color: Colors.green),
+  Category(id: 'as78dfas', title: 'Hush Puppy Ssh', color: Colors.brown),
+];
---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:meal/widgets/category_item.dart';
+import '../data/categories.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Fresh Fried Krispy'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        children: CATEGORIES.map((categoryData) {
+          return CategoryItem(categoryData.title, categoryData.color);
+        }).toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+      ),
+    );
+  }
+}
